453521598b2aeed0e795424882e7c72648a6fbc4
Thulio <thulioarrais@conselt.com.br>
2024-09-09 12:49:38 -0300

    Testano

---
+++ caTeste/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Linha 1: Teste");
+Console.WriteLine("Linha 2: Teste");
+Console.WriteLine("Linha 3: Teste");
+Console.ReadLine();
+// Especifica a linha que deseja limpar (por exemplo, a linha 2)
+int linhaParaLimpar = 1; // Índice base zero
+
+// Move o cursor para a linha especificada
+Console.SetCursorPosition(0, linhaParaLimpar);
+
+// Limpa a linha
+Console.Write(new string(' ', Console.WindowWidth));
+
+// Move o cursor de volta ao início da linha limpa
+//Console.SetCursorPosition(0, linhaParaLimpar);
+//Console.WriteLine();
+
+
+// Se desejar, pode escrever algo novo na linha
+//Console.WriteLine();
+//Console.Write("Linha 2 foi limpa!");
+Console.ReadLine();
+    
